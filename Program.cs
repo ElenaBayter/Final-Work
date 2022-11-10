7f1bed7dfc6bee8dbb40e1b75e212daddf0a9b28
elena tarasova <nafanichka.ev@gmail.com>
2022-11-10 16:55:29 -0500

    made method to find strings with length 3 or less

--- Program.cs
+++ Program.cs
@@ -2,3 +2,16 @@
 string[] PrimerArray = new string[] { "medicine", "14", "table", "hey", "34", "6534", "165", "sink", "hello", "ok", "low" };
 string[] NewArray = new string[PrimerArray.Length];
 
+void ArrayWithCondition(string[] PrimarArray, string[] NewArray)
+{
+    int j = 0;
+    for (int i = 0; i < PrimerArray.Length; i++)
+    {
+        Console.Write($"{PrimerArray[i]} ");
+        if (PrimerArray[i].Length <= 3)
+        {
+            NewArray[j] = PrimerArray[i];
+            j++;
+        }
+    }
+}
